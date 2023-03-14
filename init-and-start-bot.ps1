7246f6d0e1625ccbd5e2246a2edcf367e31717e8
Aurimas <aurimas.matiekus@gmail.com>
2023-03-15 00:01:07 +0200

    config changes

---
+++ init-and-start-bot.ps1
@@ -0,0 +1,21 @@
+cls
+
+$currentFolder = split-path -parent $Script:MyInvocation.MyCommand.Path
+
+$currentFolder
+
+set-location $currentFolder
+
+npm install
+
+cls
+
+$currentFolder = get-location
+
+write-host `n
+
+write-host $currentFolder
+
+write-host `n
+
+node watcher.js
